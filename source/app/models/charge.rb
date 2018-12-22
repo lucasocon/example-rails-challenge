aee15995655ac95a510a58049696ef2417c18740
Lucas Ocon <lucasocon@MacBook-Pro-de-Lucas.local>
2018-12-22 16:24:14 -0300

    Add json structure for charges

--- source/app/models/charge.rb
+++ source/app/models/charge.rb
@@ -1,3 +1,15 @@
 class Charge < ApplicationRecord
   belongs_to :customer
+
+  def as_json
+    {
+      id: id,
+      created: created_at.to_i
+      paid: paid,
+      amount: amount,
+      currency: currency,
+      refunded: refunded,
+      customer_id: customer_id
+    }
+  end
 end
