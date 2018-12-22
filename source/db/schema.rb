69580b415f1fe99ad7b054ced5a1fd10533363a6
Lucas Ocon <lucasocon@MacBook-Pro-de-Lucas.local>
2018-12-22 16:05:43 -0300

    Add customer model

--- source/db/schema.rb
+++ source/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_190320) do
+ActiveRecord::Schema.define(version: 2018_12_22_190509) do
 
   create_table "charges", force: :cascade do |t|
     t.boolean "paid"
     t.integer "amount"
     t.string "currency"
     t.boolean "refunded"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
