0d4fac602b04c82d57407620f782a19fa7270c58
Lucas Ocon <lucasocon@MacBook-Pro-de-Lucas.local>
2018-12-26 12:00:20 -0300

    Add more specs

--- source/spec/models/charge_spec.rb
+++ source/spec/models/charge_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Charge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it "belongs to customer" do
+      assc = described_class.reflect_on_association(:customer)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
 end
